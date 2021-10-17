c34e890bbe10269765de411efa35f92130b690ab
Vishal06gupta78 <vishal06gupta78@gmail.com>
2021-10-17 21:02:34 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des:  'Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.',
+        image: 'Images/slider 2.png'
+    },
+    {
+        name: 'loki',
+        des: 'Loki, the God of Mischief, steps out of his brother shadow to embark on an adventure that takes place after the events of Avengers Endgame.',
+        image: 'Images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.',
+        image: 'Images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.',
+        image: 'Images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.',
+        image: 'Images/slider 5.PNG'
+    }
+]
